3730583989a0f75199bb1cf2b49706603f71d3ea
Tim <tim.kreitner@googlemail.com>
2018-11-27 12:42:22 +0100

    split fuel flow
    specific vs total fuel

--- badapy/classes/airplane.py
+++ badapy/classes/airplane.py
@@ -1,21 +1,35 @@
 from ..misc.load_data import *
 from os import path
-class airplane:
-    def __init__(self, type, model):
-        self.type = type
+
+
+class Airplane:
+    def __init__(self, plane_type, model):
+        self.type = plane_type
         self.model = model
 
+        # Information from OPF File
+        self.masses = None
+        self.flight_env = None
+        self.wing_area = None
+        self.config = None
+        self.engine = None
+        self.fuel = None
+
+        # Information from AFP File
+        self.proc_climb = None
+        self.proc_cruise = None
+        self.proc_desc = None   
 
     def load_information(self, file_path):
         data_folder = path.join(file_path)
 
-        self.masses, self.flight_env, self.wing_area, self.config, self.engine, self.fuel = split_opf(path.join(data_folder, self.type, "__.OPF")) # add path
-        self.proc_climb, self.proc_cruise, self.proc_desc = split_afp(path.join(data_folder, self.type, "__.AFP"))
+        self.masses, self.flight_env, self.wing_area, self.config, self.engine, self.fuel = split_opf(
+            path.join(data_folder, self.type, "__.OPF"))  # add path
 
+        self.proc_climb, self.proc_cruise, self.proc_desc = split_afp(path.join(data_folder, self.type, "__.AFP"))
 
     def __str__(self):
         pass
-
 
     def __repr__(self):
         pass
--- badapy/classes/flight.py
+++ badapy/classes/flight.py
@@ -1,4 +1,4 @@
-class flight():
+class Flight:
     def __init__(self, tailsign, flightno, flightdata, used_plane):
         self.tailsign = tailsign
         self.flightno = flightno
@@ -11,33 +11,39 @@ class flight():
     def __str__(self):
         pass
 
-    def calculate_fuel(self, method_used='minimum'):  # TODO: Add iterator for individual waypoints.
-        """
-        Calculates the specific fuel consumption [kg/min] to then calculate the total fuel used
-        :param method_used: Choice of "thrust", "minimum", "cruise"
-        :return: fuel_flow:
-        """
-        if method_used not in ['thrust', 'minimum', 'cruise']:
-            raise ValueError('Invalid calculation method. Expected one of: %s' % ['thrust', 'minimum', 'cruise'])
-
+    def calculate_specific_fuel(self, method_used, i):
         thrust_spec_fuel_flow = self.used_plane.fuel['cf_1'] * (
-                    1 + (self.flightdata.airspeed / self.used_plane.fuel['cf_2']))
+                1 + (self.flightdata.airspeed / self.used_plane.fuel['cf_2']))
+
         if method_used == 'thrust':  # Thrust specific fuel flow
             nom_fuel_flow = thrust_spec_fuel_flow * self.flightdata.thrust
             return nom_fuel_flow
 
         elif method_used == 'minimum':  # Minimum fuel flow
-            min_fuel_flow = self.used_plane.fuel['cf_3'] * (1 - (self.flightdata.height / self.used_plane.fuel['cf_4']))
+            min_fuel_flow = self.used_plane.fuel['cf_3'] * (
+                        1 - (self.flightdata.height / self.used_plane.fuel['cf_4']))
             return min_fuel_flow
 
         else:  # Cruise fuel flow
             cruise_fuel_flow = thrust_spec_fuel_flow * self.flightdata.thrust * self.used_plane.fuel['cf_cr']
             return cruise_fuel_flow
 
-    def calculate_distance(self):
-        pass
+    def calculate_fuel(self, method_used='minimum', sampling_rate=1):  # TODO: Add iterator for individual waypoints.
+        """
+        Calculates the specific fuel consumption [kg/min] to then calculate the total fuel used
+        :param method_used: Choice of "thrust", "minimum", "cruise"; sampling_rate: time between waypoints [min]
+        :return: fuel_sum: total fuel used [kg]
+        """
+        if method_used not in ['thrust', 'minimum', 'cruise']:
+            raise ValueError('Invalid calculation method. Expected one of: %s' % ['thrust', 'minimum', 'cruise'])
+        fuel_sum = 0
 
-    def waypoint_iterator(self):
+        for i in range(self.flightdata.shape[0]):
+            fuel_sum += self.calculate_specific_fuel(method_used, i) * sampling_rate
+
+        return fuel_sum
+
+    def calculate_distance(self):
         pass
 
     def show_waypoints(self):
